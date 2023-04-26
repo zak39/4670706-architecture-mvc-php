c8e0a5cbe38f46e8fa1012f5e0457c244b7b2102
zak39 <fotia.baptiste@hotmail.com>
2023-04-26 22:42:27 +0200

    Separation en 2 fichiers
    On a separer notre fichier index.php en 2 fichiers :
    
    - index.php : Préparer les données via notre requête SQL qui inclus
      notre template.
    - templates/homepage.php : C'est notre fichier qui sert uniquement à
      présenter les données.

--- blog/index.php
+++ blog/index.php
@@ -25,41 +25,6 @@
 
         $posts[] = $post;
     }
+
+    require('templates/homepage.php');
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Le blog de l'AVBN</title>
-        <link href="style.css" rel="stylesheet" />
-    </head>
-
-    <body>
-        <h1>Le super blog de l'AVBN !</h1>
-        <h2>p1c3/premiere-separation</h2>
-        <p>Derniers billets du blog :</p>
-
-        <?php
-            foreach ($posts as $post) {
-        ?>
-            <div class="news">
-                <h3>
-                    <?php echo(htmlspecialchars($post['title'])); ?>
-                    <em>le <?php echo($post['french_creation_date']); ?></em>
-                </h3>
-                <p>
-                    <?php
-                        // We display the post content.
-                        echo(nl2br((htmlspecialchars( $post['content']))));
-                    ?>
-                    <br />
-                    <em><a href="#">Commentaires</a></em>
-                </p>
-            </div>
-        <?php
-            } // The end of the posts loop.
-            //$statement->closeCursor();
-        ?>
-    </body>
-</html>
