835aefb426b05e04f29dcf88954ffac8587f65a4
Baptiste Fotia <fotia.baptiste@hotmail.com>
2023-05-06 15:17:20 +0200

    Creation de la classe Database (composition)
    Création de la classe Database tiré du chapitre sur la composition du
    cours MVC.
    
    Signed-off-by: Baptiste Fotia <fotia.baptiste@hotmail.com>

--- blog/src/controllers/post.php
+++ blog/src/controllers/post.php
@@ -1,13 +1,17 @@
 <?php
 
+require_once('src/lib/database.php');
 require_once('src/model/post.php');
 require_once('src/model/comment.php');
 
 function post(string $identifier)
 {
     $postRepository = new PostRepository();
+    $postRepository->connection = new DatabaseConnection();
     $post = $postRepository->getPost($identifier);
-    $comments = getComments($identifier);
+    $commentRepository = new CommentRepository();
+    $commentRepository->connection = new DatabaseConnection();
+    $comments = $commentRepository->getComments($identifier);
 
     require('templates/post.php');
 }
